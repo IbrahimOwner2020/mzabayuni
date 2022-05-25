8579f8ef742792b7730b7d1a708ae571f38b40fe
IbrahimOwner2020 <kidibra2020@gmail.com>
2022-05-25 12:22:19 +0300

    feat: tawk-chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,39 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
-import { ToastContainer } from 'react-toastify'
+import "tailwindcss/tailwind.css";
+import React, { useEffect } from "react";
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import Tawkto from "tawkto-react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <ToastContainer />
-      <Component {...pageProps} />
-    </div>
-  )
+	useEffect(() => {
+		var tawk = new Tawkto("628de703b0d10b6f3e73edb1", "1g3t4stkc");
+
+		tawk.onStatusChange((status) => {
+			console.log(status);
+		});
+	}, []);
+
+	return (
+		<div>
+			<ToastContainer />
+			<Component {...pageProps} />
+		</div>
+	);
 }
-export default MyApp
+export default MyApp;
+
+// TALW SETTINGS
+
+// <!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/628de703b0d10b6f3e73edb1/1g3t4stkc';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+// </script>
+// <!--End of Tawk.to Script-->
